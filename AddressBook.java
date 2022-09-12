b5d9906f4fbe9db5ce553ceb4ae93abb3ec7eb48
sunilmsd007 <sunilmsd007@gmail.com>
2022-09-12 22:24:52 +0530

    created an Address Book class

---
+++ AddressBook.java
@@ -0,0 +1,24 @@
+package addressBookProgram;
+import java.util.*;
+
+public class AddressBook {
+
+public String firstName,lastName,address,state,city,zip,phoneNumber,email;
+	    
+	    public AddressBook(String Fname,String Lname,String address,String city,String state,String zip,String phoneNumber,String email)
+	    {
+	        this.firstName = Fname;
+	        this.lastName = Lname;
+	        this.address=address;
+	        this.city=city;
+	        this.state=state;
+	        this.zip=zip;
+	        this.phoneNumber=phoneNumber;
+	        this.email=email;
+	    }
+         
+	public static void main(String[] args)  
+	    {
+	
+             }
+}
