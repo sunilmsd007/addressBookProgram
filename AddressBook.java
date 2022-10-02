5fd7e20672718231e5fa97e51477b5c4bd5a0182
sunilmsd007 <sunilmsd007@gmail.com>
2022-10-02 11:58:48 +0530

    modified: ability to add multiple contacts address book

--- AddressBook.java
+++ AddressBook.java
@@ -2,7 +2,31 @@ package addressBookProgram;
 import java.util.*;
 
 	class Person{
-	   ArrayList<String> list;
+	String firstname, lastname, address, city, state, zip, phoneNumber, email;
+
+	Person(String firstname, String lastname, String address, String city, String state, String zip, String phoneNumber,
+			String email) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+        void display() {
+		System.out.println("First Name: " + firstname + " Last Name: " + lastname + " Address: " + address + " City: "
+				+ city + "  State: " + state + " Zip: " + zip + " Phone No: " + phoneNumber + " Email: " + email
+				+ "\n");
+	}
+}
+        class Book {
+	ArrayList<Person> book;
+
+	Book() {
+		book = new ArrayList<>();
+	}
          public void add() {
          //method to add person details
 		Scanner sc =new Scanner(System.in);
@@ -22,153 +46,162 @@ import java.util.*;
 		String phoneNumber=sc.next();
 		System.out.println("enter email");
 		String email=sc.next();
-       		list = new ArrayList<>();
-		list.add(firstname);
-		list.add(lastname);
-		list.add(address);
-		list.add(city);
-		list.add(state);
-		list.add(zip);
-		list.add(phoneNumber);
-		list.add(email);
-       }
-       
-        public void delete() {
-         //method to delete person details
-    	   System.out.println("enter existing data");
-    	   Scanner sc = new Scanner(System.in);
-    	   String data=sc.next();
-    	   for(String element : list) {
-    		   if(element.contains(data)) {
-    			   System.out.println(list);
-    			   list.clear();
-    			   System.out.println("removed the data");
-    			   displayDetails();
-    		   }
-    	   }
-       }
-        public void edit() {
-        //method to edit person's details
-    	   System.out.println("enter existing data");
-    	   Scanner sc = new Scanner(System.in);
-    	   String data=sc.next();
-    	   for(String element : list) {
-    		   if(element.contains(data)) {
-    			   System.out.println(list);
-    			   System.out.println("choose what to edit"); //providing user the choice to select what detail to be edited
-    			   System.out.println("1.firstName\n 2.LastName\n 3.Address\n 4.city\n 5.state\n 6.zip\n 7.phoneNumber\n 8.email");
-    			   int ch=sc.nextInt();
-    		       switch(ch) {
-    		       case 1:{
-    		    	   System.out.println("enter the data to be edited as firstname");
-    		    	   String firstname= sc.next();
-    		    	   list.set(0,firstname);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       case 2:{
-    		    	   System.out.println("enter the data to be edited as lastname");
-    		    	   String lastname= sc.next();
-    		    	   list.set(1,lastname);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       case 3:{
-    		    	   System.out.println("enter the data to be edited as address");
-    		    	   String address = sc.next();
-    		    	   list.set(2,address);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       case 4:{
-    		    	   System.out.println("enter the data to be edited as city");
-    		    	   String city= sc.next();
-    		    	   list.set(3,city);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       case 5:{
-    		    	   System.out.println("enter the data to be edited as state");
-    		    	   String state = sc.next();
-    		    	   list.set(4,state);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       case 6:{
-    		    	   System.out.println("enter the data to be edited as zip");
-    		    	   String zip= sc.next();
-    		    	   list.set(5,zip);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       case 7:{
-    		    	   System.out.println("enter the data to be edited as mobilNumber");
-    		    	   String mobile = sc.next();
-    		    	   list.set(6,mobile);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       case 8:{
-    		    	   System.out.println("enter the data to be edited as email");
-    		    	   String email = sc.next();
-    		    	   list.set(7,email);
-    		    	   System.out.println("changed data");
-        			   System.out.println(list);
-        			   break;
-    		       }
-    		       }
-    		   }
-    	   }
-       }
+       		Person s = new Person(firstname, lastname, address, city, state, zip, phoneNumber, email);
+		book.add(s);
 
-	public void displayDetails() {  //method to display details of person
-	    	if(list.size()==0)
-	    		System.out.println("Now List Is Empty");
-	    	else
-	    	System.out.println("FirstName: "+list.get(0)+ "," +" LastName: " +list.get(1)+ "," +" Address: " +list.get(2)+"," +" City: " +list.get(3)+"," +" State: " +list.get(4)+"," +" zip: " +list.get(5)+"," +" PhoneNumber: " +list.get(6)+"," +" email: " +list.get(7));
-	    }}
-
-	public class AddressBook {
-	public static void main(String[] args) throws Exception 
-	    {
-         int l;
-		Person p = new Person();
-        Scanner sc =new Scanner(System.in);	
-        do {
-		System.out.println("enter 1 for adding contacts");
-		System.out.println("enter 2 for edit ");
-		System.out.println("enter 3 for display ");
-		System.out.println("enter 4 for delete ");
-		int choice = sc.nextInt();
-		switch(choice) {
-		case 1:{
-			p.add();
-			break;
+       }
+	public void displayDetails() {
+         //method to display details of person
+		System.out.println("enter existing first name");
+		Scanner sc = new Scanner(System.in);
+		String data = sc.next();
+		for (int i = 0; i <= book.size(); i++) {
+			Person p = book.get(i);
+			if (data.equals(p.firstname)) {
+				p.display();
+			}
+			else
+				System.out.println("data not found");
 		}
-	        case 2:{
-			p.edit();
-			break;
-		}
-	        case 3:{
-	    	p.displayDetails();
-			break;
-		}
-	        case 4:{
-	    	p.delete();
-			break;
-		}
-		}System.out.println("enter 0 to go back to main menu");
-		 l=sc.nextInt();
-		 } while(l==0);
-		System.out.println("EXITED");
-		
-	    }
 	}
+	public void delete() {
+         //method to delete person's details
+		System.out.println("enter existing first name");
+		Scanner sc = new Scanner(System.in);
+		String data = sc.next();
+		for (int i = 0; i <= book.size(); i++) {
+			Person p = book.get(i);
+			if (data.equals(p.firstname)) {
+				book.clear();
+				System.out.println("data deleted");
+			}
+		}
+	}
+        public void edit() {
+         //method to edit person's details
+		System.out.println("enter existing first name");
+		Scanner sc = new Scanner(System.in);
+		String data = sc.next();
+		for (int i = 0; i <= book.size(); i++) {
+			Person p = book.get(i);
+			if (data.equals(p.firstname)) {
+				while (true) {
+					System.out.println("choose what to edit");
+					System.out.println(
+							"1.firstName\n 2.LastName\n 3.Address\n 4.city\n 5.state\n 6.zip\n 7.phoneNumber\n 8.email\n 9.Exit");
+					int ch = sc.nextInt();
+					switch (ch) {
+					case 1: {
+						System.out.println("enter the data to be edited as firstname");
+						String firstname = sc.next();
+						p.firstname = firstname;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 2: {
+						System.out.println("enter the data to be edited as lastname");
+						String lastname = sc.next();
+						p.lastname = lastname;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 3: {
+						System.out.println("enter the data to be edited as address");
+						String address = sc.next();
+						p.address = address;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 4: {
+						System.out.println("enter the data to be edited as city");
+						String city = sc.next();
+						p.city = city;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 5: {
+						System.out.println("enter the data to be edited as state");
+						String state = sc.next();
+						p.state = state;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 6: {
+						System.out.println("enter the data to be edited as zip");
+						String zip = sc.next();
+						p.zip = zip;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 7: {
+						System.out.println("enter the data to be edited as mobilNumber");
+						String mobile = sc.next();
+						p.phoneNumber = mobile;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 8: {
+						System.out.println("enter the data to be edited as email");
+						String email = sc.next();
+						p.email = email;
+						System.out.println("changed data");
+						displayDetails();
+						break;
+					}
+					case 9: {
+						System.exit(0);
+					}
+
+					}
+				}
+			}
+		}
+	}
+
+}
+
+   public class AddressBook {
+	public static void main(String[] args)
+	{
+		int l;
+		Book b = new Book();
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("enter 1 for adding contacts");
+			System.out.println("enter 2 for edit ");
+			System.out.println("enter 3 for display ");
+			System.out.println("enter 4 for delete ");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1: {
+				b.add();
+				break;
+			}
+			case 2: {
+				b.edit();
+				break;
+			}
+			case 3: {
+				b.displayDetails();
+				break;
+			}
+			case 4: {
+				b.delete();
+				break;
+			}
+			}
+			System.out.println("enter 0 to go back to main menu");
+			l = sc.nextInt();
+		} while (l == 0);
+		System.out.println("EXITED");
+
+	}
+}
 
